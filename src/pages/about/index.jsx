d25a53bb9fa4d1e40f4f3146379dd8a8ed210886
Antonio <netoifpe@gmail.com>
2023-01-20 14:21:54 -0300

    feat: create page about

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,7 +1,76 @@
+import { CardSkills } from "../../components/CardSkills";
+import { Footer } from "../../components/Footer";
+import { infosSkills } from "../../services/dataBase";
+import {
+  StyledContainerAbout,
+  StyledAboutInfos,
+  StyledAboutSkills,
+} from "./style";
+
 export const AboutPage = () => {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <StyledContainerAbout>
+      <StyledAboutInfos>
+        <div>
+          <h2>
+            <span>/</span>Sobre Mim
+          </h2>
+        </div>
+
+        <div>
+          <div>
+            <p>
+              Olá, meu nome é Antonio Santos!
+              <br></br>
+              <br></br>
+              Sou desenvolvedor Front-end , com boa experiência no
+              desenvolvimento de aplicações utilizando, React, TypeScript,
+              JavaScript, HTML e CSS, possuindo vivências em utilização de
+              Metodologias ágeis, em especial Scrum, como também em
+              versionamento de projetos, utilizando Git.
+              <br></br>
+              <br></br>
+              Atualmente sou monitor na Kenzie Academy Brasil e curso o módulo
+              de back-end do curso de Desenvolvimento Web Fullstack, da mesma
+              instituição, como também tenho buscado aprendizado e certificações
+              em outras instituições, como Freecodecamp, Rocketseat e W3schools,
+              procurando sempre aumentar a minha rede de conhecimento.
+              <br></br>
+              <br></br>
+              Amo a área de tecnologia primeiro porque ela está presente em
+              quase tudo no nosso dia-a-dia, e por me permitir colocar em
+              prática meu amor e conhecimento de lógica. Meu objetivo na área é
+              tornar-se um profissional com alto nível de competências, capaz de
+              desenvolver sempre a melhor experiência para usuário final.
+            </p>
+          </div>
+          <figure>
+            <img
+              src="/my-photo-perfil.png"
+              alt="imagem perfil Antonio Santos"
+            />
+            <img src="/geometrics1.svg" alt="" />
+            <img src="/geometrics2.svg" alt="" />
+          </figure>
+        </div>
+      </StyledAboutInfos>
+      <StyledAboutSkills>
+        <div>
+          <h2>
+            <span>#</span>Habilidades
+          </h2>
+          <span></span>
+        </div>
+
+        <div>
+          <ul>
+            {infosSkills.map((infos, index) => {
+              return <CardSkills key={index} infos={infos} />;
+            })}
+          </ul>
+        </div>
+      </StyledAboutSkills>
+      <Footer />
+    </StyledContainerAbout>
   );
 };
