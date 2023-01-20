a0fc5b5764e3890c5f97581db04f5065f2e1d3e1
Antonio <netoifpe@gmail.com>
2023-01-20 13:29:16 -0300

    feat: create component SectionInitialInfos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,5 @@
 import {
   StyledContainerHome,
-  StyledInitialInfos,
   StyledProjects,
   StyledSkills,
   StyledABout,
@@ -11,38 +10,12 @@ import { CardProject } from "../../components/CardProject";
 import { CardSkills } from "../../components/CardSkills";
 import { LinkNavigation } from "../../components/LinkNavigation";
 import { Footer } from "../../components/Footer";
+import { SectionInitialInfos } from "./SectionInitialInfos";
 
 export const HomePage = () => {
   return (
     <StyledContainerHome>
-      <StyledInitialInfos>
-        <div>
-          <h1>
-            Antonio <span>Santos</span>, Desenvolvedor <span>Front-end</span>{" "}
-          </h1>
-          <h3>
-            Sou desenvolvedor Front-end com foco em ReactJs, monitor e estudante
-            do curso de Desenvolvimento Web Full Stack da Kenzie Academy Brazil.
-          </h3>
-          <LinkNavigation
-            name="Entre em contato!"
-            to="/contact"
-            variant="InternalPrimary"
-          />
-        </div>
-
-        <div>
-          <figure>
-            <img
-              src="/my-photo-perfil.png"
-              alt="imagem perfil Antonio Santos"
-            />
-            <img src="/geometrics1.svg" alt="" />
-            <img src="/geometrics2.svg" alt="" />
-          </figure>
-        </div>
-      </StyledInitialInfos>
-
+      <SectionInitialInfos />
       <StyledProjects>
         <div>
           <h2>
@@ -99,21 +72,25 @@ export const HomePage = () => {
               Olá, meu nome é Antonio Santos!
               <br></br>
               <br></br>
-              Atualmente sou desenvolvedor Front-end, monitor na Kenzie Academy
-              Brasil e curso Desenvolvimento Web Fullstack na mesma instituição,
-              como também tenho buscado aprendizado e certificações em outras
-              instituições, como a rocketseat e w3schools, procurando sempre
-              aumentar a minha rede de conhecimento.
+              Sou desenvolvedor Front-end , com boa experiência no
+              desenvolvimento de aplicações utilizando, React, TypeScript,
+              JavaScript, HTML e CSS, possuindo vivências em utilização de
+              Metodologias ágeis, em especial Scrum, como também em
+              versionamento de projetos, utilizando Git.
               <br></br>
               <br></br>
-              Meu contato com tecnologia começou em 2009, passou por uma longa
-              pausa, e retornou em 2022, quando adentrei no curso da Kenzie
-              Academy Brasil. Amo a área de tecnologia primeiro porque ela está
-              presente em quase tudo no nosso dia-a-dia, e por me permitir
-              colocar em prática meu amor e conhecimento de lógica. Meu objetivo
-              na área é tornar-se um profissional com alto nível de
-              competências, capaz de desenvolver sempre a melhor experiência
-              para usuário final.
+              Atualmente sou monitor na Kenzie Academy Brasil e curso o módulo
+              de back-end do curso de Desenvolvimento Web Fullstack, da mesma
+              instituição, como também tenho buscado aprendizado e certificações
+              em outras instituições, como Freecodecamp, Rocketseat e W3schools,
+              procurando sempre aumentar a minha rede de conhecimento.
+              {/* <br></br>
+              <br></br>
+              Amo a área de tecnologia primeiro porque ela está presente em
+              quase tudo no nosso dia-a-dia, e por me permitir colocar em
+              prática meu amor e conhecimento de lógica. Meu objetivo na área é
+              tornar-se um profissional com alto nível de competências, capaz de
+              desenvolver sempre a melhor experiência para usuário final. */}
             </p>
             <LinkNavigation
               name="Mais informações <~>"
