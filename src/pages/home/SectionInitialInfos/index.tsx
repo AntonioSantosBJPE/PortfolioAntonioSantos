5adaaffefdfe87cc81544999307e68d4e8c352f5
Antonio <netoifpe@gmail.com>
2023-05-15 11:13:02 -0300

    fix: updating portfolio presentation text

--- src/pages/home/SectionInitialInfos/index.tsx
+++ src/pages/home/SectionInitialInfos/index.tsx
@@ -6,11 +6,13 @@ export const SectionInitialInfos = () => {
     <StyledInitialInfos>
       <div>
         <h1>
-          Antonio <span>Santos</span>, Desenvolvedor <span>Front-end</span>{" "}
+          Antonio <span>Santos</span>, Desenvolvedor <span>Full-stack</span>{" "}
         </h1>
         <h3>
-          Sou desenvolvedor Front-end com foco em ReactJs, TypeScript,
-          JavaScript, CSS e HTML.
+          Bem-vindo ao meu site de portfólio! Sou um desenvolvedor full-stack
+          com experiência em diversas tecnologias da web, como React,
+          Typescript, CSS, HTML, Node e SQL e estou entusiasmado em compartilhar
+          com você meu conhecimento e projetos neste espaço..
         </h3>
         <LinkNavigation
           name="Entre em contato!"
