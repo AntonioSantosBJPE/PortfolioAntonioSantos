502291c5ebb061ce5b72eeec381b066ab6ec18df
Antonio <netoifpe@gmail.com>
2023-05-15 21:56:11 -0300

    fix: file styles.css in folder projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import "./styles.css";
+import "./styles.module.css";
 import { EffectCoverflow, Pagination } from "swiper";
 
 export const ProjectsPage = () => {
