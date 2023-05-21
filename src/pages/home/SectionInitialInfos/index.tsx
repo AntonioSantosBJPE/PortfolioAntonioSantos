62f1a8388029a680cee920f93fb84b03b60bc359
Antonio <netoifpe@gmail.com>
2023-05-21 10:00:07 -0300

    feat: creates the profile picture animation in the home info section on the home page

--- src/pages/home/SectionInitialInfos/index.tsx
+++ src/pages/home/SectionInitialInfos/index.tsx
@@ -1,5 +1,26 @@
 import { LinkNavigation } from "../../../components/LinkNavigation";
 import { StyledInitialInfos } from "./style";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 export const SectionInitialInfos = () => {
   return (
@@ -22,11 +43,15 @@ export const SectionInitialInfos = () => {
       </div>
 
       <div>
-        <figure>
-          <img src="/my-photo-perfil.png" alt="imagem perfil Antonio Santos" />
-          <img src="/geometrics1.svg" alt="" />
-          <img src="/geometrics2.svg" alt="" />
-        </figure>
+        <motion.figure variants={container} initial="hidden" animate="visible">
+          <motion.img
+            src="/my-photo-perfil.png"
+            alt="imagem perfil Antonio Santos"
+            variants={item}
+          />
+          <motion.img src="/geometrics1.svg" alt="geometrics" variants={item} />
+          <motion.img src="/geometrics2.svg" alt="geometrics" variants={item} />
+        </motion.figure>
       </div>
     </StyledInitialInfos>
   );
